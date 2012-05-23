83395c4d0efd5a8b9df6b1189f36f252524fe6f4
dmontalvo.m <dmontalvo.m@1e2a3a45-07bc-1ec6-a7f0-eb7e609c2435>
2012-05-23 02:32:36 +0000

    Reunion 22/05/2012

--- trunk/BancoDeTesis/src/integracion/maps/ActaSustentacionMapper.java
+++ trunk/BancoDeTesis/src/integracion/maps/ActaSustentacionMapper.java
@@ -6,8 +6,13 @@ import modelo.ActaSustentacion;
 
 import org.apache.ibatis.annotations.*;
 
+
 public interface ActaSustentacionMapper {
 
 	@Select("Select * FROM actasustentacion")
 	List<ActaSustentacion> getListaActaSustentacion();
+	
+	@Insert("Insert into actasustentacion values(#{acta})")
+	void insertarActaSustentacion(@Param("acta") ActaSustentacion actaSustentacion);
+	
 }
--- trunk/BancoDeTesis/src/negocio/impl/SustentacionServiceImpl.java
+++ trunk/BancoDeTesis/src/negocio/impl/SustentacionServiceImpl.java
@@ -1,7 +1,19 @@
 package negocio.impl;
 
+import integracion.maps.ActaSustentacionMapper;
+
+import java.util.List;
+
+import modelo.ActaSustentacion;
 import negocio.SustentacionService;
 
 public class SustentacionServiceImpl implements SustentacionService {
+
+	ActaSustentacionMapper actaSustentacionMapper;
+	
+	@Override
+	public List<ActaSustentacion> getListaActaSustentacion() {
+		return actaSustentacionMapper.getListaActaSustentacion();
+	}
 
 }
